742e011f4d5add8db2bccfe3e54f6601d28206ee
Jinhui-Huang <2634692718@qq.com>
2023-10-21 16:55:56 +0800

    feat: 创建项目框架

---
+++ src/main/java/com/myhd/JsoftProject03Application.java
@@ -0,0 +1,13 @@
+package com.myhd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoftProject03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsoftProject03Application.class, args);
+    }
+
+}
