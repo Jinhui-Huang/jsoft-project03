046f1b53fe2a1c9e6ee36ceb30a23d0461c34afa
yxl <1660855825@qq.com>
2023-10-26 15:35:05 +0800

    fix:修改用户信息服务层返回值类型

--- src/main/java/com/myhd/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/myhd/service/impl/UserInfoServiceImpl.java
@@ -3,6 +3,8 @@ package com.myhd.service.impl;
 import com.myhd.entity.UserInfo;
 import com.myhd.mapper.UserInfoMapper;
 import com.myhd.service.IUserInfoService;
+import com.myhd.util.Code;
+import com.myhd.util.Result;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
@@ -22,20 +24,41 @@ public class UserInfoServiceImpl implements IUserInfoService {
     @Resource
     private UserInfoMapper userInfoMapper;
 
+    /**
+     * @description: 进行个人信息的完善插入
+     * @param userInfo 用户个人信息
+     * @return: com.myhd.util.Result
+     * @author CYQH
+     * @date: 2023/10/26 下午3:28
+     */
     @Override
-    public Boolean addUserInfo(UserInfo userInfo) {
-
-        Integer integer = null;
+    public Result addUserInfo(UserInfo userInfo) {
+        Integer integer;
         try {
             integer = userInfoMapper.saveUserInfo(userInfo);
         } catch (DuplicateKeyException e) {
-            return false;
+            return Result.fail(Code.POST_FAIL,false,"完善个人信息失败");
         }
-        return integer == 1;
+        if(integer == 1){
+            return Result.ok(Code.POST_OK,true,"完善个人信息成功");
+        }
+        return Result.fail(Code.POST_FAIL,false,"完善个人信息失败");
     }
 
+    /**
+     * @description: 根据用户编号获取用户个人信息进行数据展示
+     * @param userId 用户编号
+     * @return: com.myhd.util.Result
+     * @author CYQH
+     * @date: 2023/10/26 下午3:33
+     */
     @Override
-    public UserInfo getUserInfoById(Integer userId) {
-        return userInfoMapper.getUserInfoById(userId);
+    public Result getUserInfoById(Integer userId) {
+
+        UserInfo userInfo = userInfoMapper.getUserInfoById(userId);
+        if (userInfo != null){
+            return Result.ok(Code.GET_OK,userInfo,"获取个人信息成功");
+        }
+        return Result.fail(Code.GET_FAIL, null,"获取个人信息失败");
     }
 }
