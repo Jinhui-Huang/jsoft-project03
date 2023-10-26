3914c0510eff69e14b6788beb2aeb3523f74435c
Jinhui-Huang <2634692718@qq.com>
2023-10-26 09:43:04 +0800

    feat: 添加领域获取功能

--- src/main/java/com/myhd/service/IRecruitService.java
+++ src/main/java/com/myhd/service/IRecruitService.java
@@ -1,5 +1,7 @@
 package com.myhd.service;
 
+import com.myhd.util.Result;
+
 /**
  * <p>
  * 招聘职位表 服务类
@@ -10,4 +12,11 @@ package com.myhd.service;
  */
 public interface IRecruitService {
 
+    /**
+     * Description: getFields 服务层中获取redis中存放的相关领域
+     * @return com.myhd.util.Result
+     * @author jinhui-huang
+     * @Date 2023/10/26
+     * */
+    Result getFields();
 }
