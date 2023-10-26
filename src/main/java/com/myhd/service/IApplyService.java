dd5a4aa008c1f1040cdeaee881f42cc2ac598d93
yxl <1660855825@qq.com>
2023-10-26 10:27:31 +0800

    feat:完成申请职位服务层

--- src/main/java/com/myhd/service/IApplyService.java
+++ src/main/java/com/myhd/service/IApplyService.java
@@ -1,5 +1,10 @@
 package com.myhd.service;
 
+import com.myhd.entity.Apply;
+import com.myhd.entity.Recruit;
+
+import java.util.List;
+
 /**
  * <p>
  * 职位申请表 服务类
@@ -9,5 +14,27 @@ package com.myhd.service;
  * @since 2023-10-23
  */
 public interface IApplyService {
+
+    /**
+     * @description 点击立即申请按钮，发送信息
+     * @author JoneElmo && CYQH
+     * @date 2023-10-24 10:25
+     * @param apply 申请的数据对象
+     * @return Integer 1 成功 0 失败
+     */
+    Boolean insertApplyInfo(Apply apply);
+
+    /**
+     * @description 我的职位页面，显示所有个人已经申请的职位 功能
+     * select a.*,r.* from tb_apply a
+     * join tb_recruit r
+     * using(recruit_id,company_id)
+     * where user_id = #{userId}
+     * @author JoneElmo && CYQH
+     * @date 2023-10-24 11:05
+     * @param userId 当前用户编号
+     * @return
+     */
+    List<Recruit> getAllUserApply(Integer userId, String like);
 
 }
--- src/main/java/com/myhd/service/impl/ApplyServiceImpl.java
+++ src/main/java/com/myhd/service/impl/ApplyServiceImpl.java
@@ -1,7 +1,14 @@
 package com.myhd.service.impl;
 
+import com.myhd.entity.Apply;
+import com.myhd.entity.Recruit;
+import com.myhd.mapper.ApplyMapper;
 import com.myhd.service.IApplyService;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -14,4 +21,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class ApplyServiceImpl implements IApplyService {
 
+    @Resource
+    private ApplyMapper applyMapper;
+
+    @Override
+    public Boolean insertApplyInfo(Apply apply) {
+        Integer integer = null;
+        try {
+            integer = applyMapper.insertApplyInfo(apply);
+        } catch (DuplicateKeyException e) {
+            return false;
+        }
+        return integer == 1;
+    }
+
+    @Override
+    public List<Recruit> getAllUserApply(Integer userId, String like) {
+        return applyMapper.getAllUserApply(userId,like);
+    }
 }
