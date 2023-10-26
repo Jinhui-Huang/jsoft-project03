380c23c275b2101cbcc973b9a7333e36af2f24a9
Jinhui-Huang <2634692718@qq.com>
2023-10-26 12:50:25 +0800

    feat: 完成前端领域展示

--- src/main/java/com/myhd/service/impl/RecruitServiceImpl.java
+++ src/main/java/com/myhd/service/impl/RecruitServiceImpl.java
@@ -10,10 +10,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Field;
+import java.util.*;
 
 /**
  * <p>
@@ -32,12 +30,19 @@ public class RecruitServiceImpl implements IRecruitService {
 
     /**
      * Description: getFields 服务层中获取redis中存放的相关领域
+     * <br></br>
      * 101~104: 销售 | 市场 | 客服 | 贸易
+     * <br></br>
      * 201~204: 经验管理 | 人事 | 行政 | 财务
+     * <br></br>
      * 301~304: 生产 | 质管 | 技工 | 物流
+     * <br></br>
      * 401~404: 教育 | 法律 | 咨询 | 翻译
+     * <br></br>
      * 501~504: 零售 | 家政 | 餐饮 | 旅游
+     * <br></br>
      * 601~604: 广告 | 媒体 | 艺术 | 出版
+     * <br></br>
      * 701~703: IT | 互联网 | 通信
      * @return com.myhd.util.Result
      * @author jinhui-huang
@@ -48,11 +53,28 @@ public class RecruitServiceImpl implements IRecruitService {
         String key = "job:cache:filed";
         Map<Object, Object> entries = stringRedisTemplate.opsForHash().entries(key);
         List<Filed> list = new ArrayList<>();
-        entries.forEach((o, o2) -> {
-            int jobFiled = Integer.parseInt(String.valueOf(o));
-            String filedName = String.valueOf(o2);
-            list.add(new Filed(jobFiled, filedName));
+        entries.forEach((o1, o2) -> {
+            list.add(new Filed(Integer.parseInt(String.valueOf(o1)), String.valueOf(o2)));
         });
-        return new Result(Code.GET_OK, list, "全领域展示在前端");
+        list.sort(Comparator.comparingInt(Filed::getJobFiled));
+
+        List<List<Filed>> resultList = new ArrayList<>();
+        List<Filed> tempList = new ArrayList<>();
+        int num1 = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (i == list.size() - 1) {
+                resultList.add(tempList);
+            }
+            if (num1 == 4) {
+                resultList.add(tempList);
+                tempList = new ArrayList<>();
+                num1 = 0;
+                i--;
+            } else {
+                tempList.add(list.get(i));
+                num1++;
+            }
+        }
+        return new Result(Code.GET_OK, resultList, "全职位领域展示在前端");
     }
 }
