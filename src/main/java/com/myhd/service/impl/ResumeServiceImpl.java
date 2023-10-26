d09254004a8569b995b43bd5bb274fd4dc3a7459
JoneElmo <mhangggggg@outlook.com>
2023-10-26 10:03:22 +0800

    feat： 简历业务实现 v1.0

--- src/main/java/com/myhd/service/impl/ResumeServiceImpl.java
+++ src/main/java/com/myhd/service/impl/ResumeServiceImpl.java
@@ -5,15 +5,43 @@ import com.myhd.mapper.ResumeMapper;
 import com.myhd.service.IResumeService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 /**
  * <p>
  * 简历表 服务实现类
  * </p>
  *
- * @author Jinhui-Huang
+ * @author JoneElmo
  * @since 2023-10-23
  */
 @Service
 public class ResumeServiceImpl implements IResumeService {
 
+    @Resource
+    ResumeMapper resumeMapper;
+
+    /**
+     * @description
+     * @author JoneElmo
+     * @date 2023-10-26 09:52
+     * @param userId
+     * @return Resume
+     */
+    @Override
+    public Resume acquireResumeInfo(Integer userId) {
+        return resumeMapper.getResumeByUserId(userId);
+    }
+
+    /**
+     * @description
+     * @author JoneElmo
+     * @date 2023-10-26 09:53
+     * @param resume
+     * @return Boolean
+     */
+    @Override
+    public Boolean saveResumeInfo(Resume resume) {
+        return resumeMapper.insertResume(resume) ==1 ?true:false;
+    }
 }
