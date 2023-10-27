1bc181eb889c98bdbf2e34d0d7044aacb7438f3f
yxl <1660855825@qq.com>
2023-10-27 10:11:49 +0800

    feat:添加用户信息controller方法

--- src/main/java/com/myhd/controller/UserInfoController.java
+++ src/main/java/com/myhd/controller/UserInfoController.java
@@ -43,6 +43,7 @@ public class UserInfoController {
 
     @PostMapping
     public Result addUserInfo(@RequestBody UserInfo userInfo){
+        System.out.println(userInfo);
         return userInfoService.addUserInfo(userInfo);
     }
 
