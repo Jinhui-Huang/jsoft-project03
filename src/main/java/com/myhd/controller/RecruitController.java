d639c2b632368af9e05f7e60d5b30737442d09aa
JoneElmo <mhangggggg@outlook.com>
2023-10-29 15:58:43 +0800

    feat: 新增模糊查询控制器

--- src/main/java/com/myhd/controller/RecruitController.java
+++ src/main/java/com/myhd/controller/RecruitController.java
@@ -1,15 +1,17 @@
 package com.myhd.controller;
 
 
+import cn.hutool.json.JSONUtil;
 import com.myhd.service.IRecruitService;
+import com.myhd.util.Code;
 import com.myhd.util.Result;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.websocket.server.PathParam;
 
 /**
  * <p>
@@ -21,11 +23,15 @@ import javax.annotation.Resource;
  */
 @RestController
 @RequestMapping("/recruit")
+@SuppressWarnings("all")
+@Slf4j
 public class RecruitController {
 
     @Resource
     private IRecruitService recruitService;
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
     /**
      * Description: getFields 获取Redis里面存储的相关领域
      * @return com.myhd.util.Result
@@ -45,103 +51,34 @@ public class RecruitController {
      * @author CYQH
      * @date: 2023/10/28 下午4:35
      */
-
-    /*require('@/../dist/static/images/20.PNG' )
-    import axios from 'axios';
-export default {
-    data() {
-        return {
-            companyIcon: "",//企业图标
-            companyName: "",//企业名称
-            companyType: "",//企业性质
-            companyField: "",//企业行业
-            companyScale: "",//企业规模
-            companyHome: "",//企业主页
-            companyAddress: "",//企业地址
-            companyText: "",//企业介绍
-
-            recruitName: "",//职位名称
-            recruitTag: "",//招聘标签
-            recruitAddress: "",//职位地址
-            salary: "",//薪水
-            recruitNumber: "",//招聘人数
-            recruitDegree: "",//招聘学历
-            recruitAge: "",//招聘年龄
-            recruitExp: "",//工作经验
-            recruitType: "",//招聘方式
-            recruitTime: "",//更新时间
-            recruitTextDuty: "",//岗位职责
-            recruitTextNeed: ""//岗位要求
-        }
-    },
-    methods: {
-
-    },
-    mounted() {
-        let that = this;
-        axios({
-            method: 'GET',
-            url: '/api/company/getCompanyInfo/200001' //你的后端路径
-        })
-            .then(response => {
-                let data = response.data
-                let code = data.code
-                let msg = data.msg
-                let info = data.object
-                if (code === 200001) { //判断你的请求是否成功
-                    console.log(data)
-                    // that.companyIcon = require('@/../dist/static/images/' + info.companyIcon)
-                    that.companyName = info.companyName
-                    that.companyType = info.companyType
-                    that.companyField = info.companyField
-                    that.companyScale = info.companyScale
-                    that.companyHome = info.companyHome
-                    that.companyAddress = info.companyAddress
-                    that.companyText = info.companyText
-                } else {
-                    alert(msg)
-                }
-            }, error => {
-                console.log('错误', error.message)
-                // alert(error.message)
-            })
-        axios({
-            method: 'GET',
-            url: '/api/recruit/getRecruitInfo/200001/300001/100001' //你的后端路径
-        })
-            .then(response => {
-                let data = response.data
-                let code = data.code
-                let msg = data.msg
-                let info = data.object
-                if (code === 200001) { //判断你的请求是否成功
-                    console.log(data)
-                    that.recruitName = info.recruitName
-                    that.recruitTag = info.recruitTag
-                    that.recruitAddress = info.recruitAddress
-                    that.salary= info.recruitSalaryMin+"K"+info.recruitSalaryMax+"K[参考工资]"
-                    that.recruitNumber = info.recruitNumber
-                    that.recruitDegree = info.recruitDegree
-                    that.recruitAge = info.recruitAge
-                    that.recruitExp = info.recruitExp
-                    that.recruitType= info.recruitType
-                    that.recruitTime= info.recruitTime
-                    that.recruitTextDuty= info.recruitTextDuty
-                    that.recruitTextNeed= info.recruitTextNeed
-                } else {
-                    alert(msg)
-                }
-            }, error => {
-                console.log('错误', error.message)
-                // alert(error.message)
-            })
-
-    },
-}
-</script>*/
     @GetMapping("getRecruitInfo/{companyId}/{recruitId}/{userId}")
     public Result getRecruitInfo(@PathVariable Integer companyId, @PathVariable Integer recruitId, @PathVariable Integer userId){
         return recruitService.acquireRecruitInfo(companyId,recruitId,userId);
     }
 
+    /**
+     * @description 首页模糊查询职位列表   非快捷查询
+     * @author JoneElmo
+     * @date 2023-10-29 09:58
+     * @param word
+     * @return Result
+     */
+    @GetMapping("/queryRecruitList")
+    public Result QueryRecruitList(@PathParam("word") String word, @PathParam("pageNum") Integer pageNum){
+        val s = stringRedisTemplate.opsForValue().get("index:" + word + ":"+pageNum);
+        System.out.println(word);
+        System.out.println(pageNum);
+        if (s==null){
+            log.info("redis中不存在数据，正在从数据库查询...");
+            val result = recruitService.searchRecruit(false, word, pageNum);
+            return result;
+        }else {
+            log.info("redis中存在数据，正在返回结果...");
+            log.info("从redis查到的职位列表信息："+s);
+            val jsonObject = JSONUtil.parseObj(s);
+            log.info("转换为json对象的数据"+jsonObject);
+
+            return new Result(Code.GET_OK,jsonObject,"获取职位列表成功");
+        }
+    }
 }
